928cb613a4479b1c4820bdf411a1afd91a882f8b
kiwoonjung <kiwoon0627@gmail.com>
2025-01-09 13:50:48 -0800

    20240109 13:50 fixed useMemo error

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
-import { themeSettings } from "theme";
+import { themeSettings } from "./theme";
 
 function App() {
   const mode = useSelector((state) => state.mode);
-  const theme = useMemo(() => createTheme(themeSettings(mode)), mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   return (
     <div className="app">
